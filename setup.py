8d0521767d91cbee49a8ab02e7d4364273d4d23d
md-naim-molla <naim.molla.stats@gmail.com>
2026-06-10 17:02:12 +0600

    change the folder directory

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'medical_chatbot',
+    version= '0.1.0',
+    author = 'Md Naim Molla',
+    author_email='naim.molla.stats@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
